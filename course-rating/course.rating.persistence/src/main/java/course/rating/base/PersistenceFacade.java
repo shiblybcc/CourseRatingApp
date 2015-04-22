4960c7cf2c99b83d21ee67b8c2988d126d8de076
Junior Lekane <junior.lekane.nimpa@rwth-aachen.de>
2015-04-22 15:51:45 +0200

    some changes in persistence facade...

--- course-rating/course.rating.persistence/src/main/java/course/rating/base/PersistenceFacade.java
+++ course-rating/course.rating.persistence/src/main/java/course/rating/base/PersistenceFacade.java
@@ -1,8 +1,11 @@
 package course.rating.base;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.ejb.Local;
+
+import com.google.common.base.Optional;
 
 import course.rating.domain.specification.Lecture;
 
@@ -15,5 +18,46 @@ import course.rating.domain.specification.Lecture;
 @Local
 public interface PersistenceFacade {
 
+	
+	/**
+	 * @return all available lectures.
+	 */
 	public List<Lecture> getAllLectures();
+	
+	/**
+	 * Gets the lecture with the given name.
+	 * 
+	 * @param lectureName   a string
+	 * @return  an object or null if no data available
+	 */
+	public Optional<Lecture> getLecture(String lectureName);
+	
+	/**
+	 * Add a new comment or sub-comment. This method works as follows:
+	 * 
+	 * 1-) If for the given lecture name, no data is available in the DB, then new resources
+	 *     will be allocated. If data are available, then the new (sub-)comment will be attached
+	 *     to the lecture data.
+	 * 2) If for a lecture name, there is already a comment with the given title, then the data being
+	 *    send will be interpreted as sub-comment.
+	 * 3) If for a lecture name, there is no comment with the given title, then a new comment will be
+	 *    attached to the lecture.
+	 *    
+	 * @param lectureName    a string which should denotes a lecture
+	 * @param commentTitle   a string as title
+	 * @param content        the content of the comment
+	 */
+	public void addComment(String lectureName, String title, String content);
+	
+	/**
+	 * Updates the statistics about a lecture.
+	 * 
+	 * @param lectureName  a string which should denotes a lecture
+	 * @param rating       the rating to add to the actual rating
+	 * @param statistics   Some statistics (Category name to value)
+	 */
+	//TODO implements me...
+	public void updateLectureRating(String lectureName, double rating, Map<String,Integer> statistics);
+	
+	
 }
--- course-rating/course.rating.persistence/src/main/java/course/rating/base/PersistenceFacadeImpl.java
+++ course-rating/course.rating.persistence/src/main/java/course/rating/base/PersistenceFacadeImpl.java
@@ -1,13 +1,21 @@
 package course.rating.base;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 import javax.ejb.EJB;
 import javax.ejb.Local;
 import javax.ejb.Stateless;
 
+import com.google.common.base.Optional;
+import com.google.common.collect.Lists;
+
 import course.rating.dao.GlobalDao;
+import course.rating.domain.specification.Comment;
 import course.rating.domain.specification.Lecture;
+import course.rating.domain.specification.SubComment;
+import course.rating.entities.LectureEntity;
 import course.rating.util.DomainObjectFactory;
 import course.rating.util.EntityBuilder;
 
@@ -17,22 +25,71 @@ import course.rating.util.EntityBuilder;
  * @author TODO...
  *
  */
+//TODO save state of object....
 @Local(PersistenceFacade.class)
 @Stateless
 public class PersistenceFacadeImpl implements PersistenceFacade{
 
 	@EJB
-	private EntityBuilder entityBuilder;
+	private DomainObjectFactory domainFactory;
 	@EJB
-	private DomainObjectFactory factory;
-	
+	private EntityBuilder entityBuilder;
 	@EJB
 	private GlobalDao dao;
 	
-	//TODO dao...
-	
+	public PersistenceFacadeImpl(){
+	}
+
 	public List<Lecture> getAllLectures() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Lecture> resultList = Lists.newArrayList();
+		List<LectureEntity> list = dao.getAllLectureEntities();
+		for(LectureEntity entity : list){
+			entity.setIsNewEntity(false);
+			resultList.add(domainFactory.create(entity));
+		}
+		return Collections.unmodifiableList(resultList);
+	}
+
+	public Optional<Lecture> getLecture(String lectureName) {
+		Lecture lecture = null;
+		Optional<LectureEntity> optional = dao.getLectureEntityWithUniqueName(lectureName);
+		if(optional.isPresent()){
+			optional.get().setIsNewEntity(false);
+			lecture = domainFactory.create(optional.get());
+		}
+		return Optional.fromNullable(lecture);
+	}
+
+	public void addComment(String lectureName, String title, String content) {
+		Optional<Lecture> optionalLecture = getLecture(lectureName);
+		Lecture lecture;
+		
+		if(optionalLecture.isPresent()){//lecture with the given name is know...
+			lecture = optionalLecture.get();
+			if(lecture.hasCommentWithTitle(title)){
+				Comment comment = lecture.getCommentWithTitle(title);
+				SubComment subComment = domainFactory.create(entityBuilder.createSubCommentEntity());
+				comment.addSubComment(subComment);
+				subComment.setContent(content)
+				.save(); // rely on the cascade mechanism...
+ 				//lecture.save();
+			}else{
+				Comment comment = domainFactory.create(entityBuilder.createCommentEntity());
+				comment.setTitle(title).setContent(content)
+				.save(); // rely on the cascade mechanism...
+				//lecture.save();
+			}
+		}else{
+			//New lecture
+			lecture = domainFactory.create(entityBuilder.createLectureEntity());
+			Comment comment = domainFactory.create(entityBuilder.createCommentEntity());
+			lecture.addComment(comment);
+			lecture.save();
+		}
+	}
+
+	public void updateLectureRating(String lectureName, double rating, Map<String, Integer> statistics) {
+		// TODO implements me....
+		
 	}
 }
